2031218771d9677c5d715a6445fc23d7e2130eed
Matt Adam <34516690+canad1an@users.noreply.github.com>
2022-11-29 11:00:19 -0800

    Update pagination_example.py

--- pagination_example.py
+++ pagination_example.py
@@ -1,3 +1,20 @@
+#!/usr/bin/python3.6
+import requests
+import urllib3
+from avi.sdk.avi_api import ApiSession
+import json
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+#############################################################################
+################################ AVI Login ##################################
+#############################################################################
+
+faileddevices = ['']
+def avi(dev):
+	api = ApiSession.get_session(dev, "serviceuser", "password  ", tenant="*", timeout=5, api_version="18.2.8")
+	return api
+
 def getavicontent(request):
     pagesize = "5"
     nextpagination = 1
@@ -14,3 +31,53 @@ def getavicontent(request):
             nextpagination = 0
 
     return datadic
+
+def delete(request):
+    api = avi(device)
+    data = api.delete(str(request))
+    datai = data.json()['results']
+    return datai
+devices=['10.206.114.19']
+
+
+#############################################################################
+######################## Update SE Virtual Services #########################
+#############################################################################
+
+for device in devices: ############ LOOP DEVICES ############
+	if device in faileddevices:
+		continue
+	try:	
+		virtuals = getavicontent('virtualservice')
+		
+		for vs in virtuals
+			deleteme = delete(vs["uuid"])
+			print (deleteme)
+	except Exception as e:
+		faileddevices.append(device)
+
+#############################################################################
+########################### Update SE Pool Groups ###########################
+#############################################################################
+
+for device in devices: ############ LOOP DEVICES ############
+	if device in faileddevices:
+		continue
+	try:
+		poolgroups = getavicontent('poolgroup')
+		print (poolgroups)
+	except Exception as e:
+		faileddevices.append(device)
+
+#############################################################################
+############################### Update SE Pools #############################
+#############################################################################
+
+for device in devices: ############ LOOP DEVICES ############
+	if device in faileddevices:
+		continue
+	try:
+		pools = getavicontent('pool')
+		print (pools)
+	except Exception as e:
+		faileddevices.append(device)
